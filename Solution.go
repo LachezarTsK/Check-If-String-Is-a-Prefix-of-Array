f6b1e35a7dea11d856a14b7c2f1742960540a09e
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-06-11 09:36:52 +0200

    Create Solution.go

---
+++ Solution.go
@@ -0,0 +1,23 @@
+
+package main
+
+import (
+	"fmt"
+)
+
+func isPrefixString(prefix string, words []string) bool {
+	indexPrefix := 0
+	sumLengthCompletedWords := 0
+
+	for i := 0; i < len(words) && indexPrefix < len(prefix); i++ {
+		for j := 0; j < len(words[i]) && indexPrefix < len(prefix); j++ {
+			if prefix[indexPrefix] != words[i][j] {
+				return false
+			}
+			indexPrefix++
+		}
+		sumLengthCompletedWords += len(words[i])
+	}
+
+	return len(prefix) == sumLengthCompletedWords
+}
